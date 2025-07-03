bb2cf280f44ab0b9cca9457f534c464fad1f4c19
timothyleconte <marktbooher@outlook.com>
2025-07-03 00:15:43 -0400

    Create working eventListener
    EventListener now working; however, still need to figure out how to make it like an etch a sketch; not functioning the way
    I'm intending it to.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@ const container = document.querySelector (".container")
 function grid(){
     for (let i = 1; i < 257; i++){
         const div = document.createElement("div");
+        div.addEventListener("click", () =>{
+            div.style.backgroundColor = "blue";
+        })
         div.style.flexBasis = "6.25%";
         div.textContent = "test";
         container.appendChild(div);
