8aa5de895c849e2584344fced89e4bcf3f392b1e
timothyleconte <marktbooher@outlook.com>
2025-07-02 23:43:27 -0400

    Create code to make divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 const container = document.querySelector (".container")
+function grid(){
+    for (let i = 1; i < 257; i++){
+        const div = document.createElement("div");
+        container.appendChild(div);
+    }
+ 
+}
+
+grid()
