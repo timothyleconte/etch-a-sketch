4d83438ad69e6654ce2d5b2b776382e3e6c50908
timothyleconte <marktbooher@outlook.com>
2025-07-03 00:37:31 -0400

    Add aspect ratio; create square

--- javascript.js
+++ javascript.js
@@ -5,8 +5,10 @@ function grid(){
         div.addEventListener("mouseover", () =>{
             div.style.backgroundColor = "blue";
         })
-        div.style.flexBasis = "6.25%";
-        div.textContent = "test";
+        div.style.flexBasis = "6%";
+        // div.textContent = "test";
+        div.style.padding = "1px";
+        // div.style.aspectRatio = "1/1";
         container.appendChild(div);
     }
  
