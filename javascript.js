4f47b53b27211456386191c832b1a3f509504747
timothyleconte <marktbooher@outlook.com>
2025-07-03 00:16:57 -0400

    Add working mouseover feature
    It helps if you use the correct syntax

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const container = document.querySelector (".container")
 function grid(){
     for (let i = 1; i < 257; i++){
         const div = document.createElement("div");
-        div.addEventListener("click", () =>{
+        div.addEventListener("mouseover", () =>{
             div.style.backgroundColor = "blue";
         })
         div.style.flexBasis = "6.25%";
