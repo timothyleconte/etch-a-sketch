b9a85543c703e9c45463ec225b976542374e69b4
timothyleconte <marktbooher@outlook.com>
2025-07-03 19:50:36 -0400

    Add logic for changing grid size
    Reworking grid size to work with user input data for next commit

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const container = document.querySelector (".container");
-function grid(){
-    for (let i = 1; i < 257; i++){
+function grid(size){
+    for (let i = 1; i < size; i++){
         const div = document.createElement("div");
         div.addEventListener("mouseover", () =>{
             div.style.backgroundColor = "blue";
@@ -11,17 +11,19 @@ function grid(){
  
 }
 
-grid();
+grid(257);
 
 const button = document.querySelector(".selectSize");
 
 button.addEventListener("click", () => {
 
-    let size = prompt("What grid size would you like?", "maximum value of 100")
+    let size = prompt("What grid size would you like?", "Enter a value between 1-100")
 
     if (size > 100){
         alert("Value is too large, please enter a max value of 100")
-    }else if{
+    }else if (size < 1){
+        alert("Value is too small. Please enter a minimum value of 1")
+    }else{
         
     }
 })
