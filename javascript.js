ac02d3a5d2ea952604cd6ca4c399df60b0193ed7
timothyleconte <marktbooher@outlook.com>
2025-07-04 14:39:33 -0400

    Add grid size selector to rainbow modified
    Rainbow mode working fully; can select grid size & deletes old divs

--- javascript.js
+++ javascript.js
@@ -77,7 +77,24 @@ function gridRainbow(size){
 const rainbowMode = document.querySelector(".rainbowMode");
 
 rainbowMode.addEventListener("click", () => {
+    
+    let size = prompt("What grid size would you like?", "Enter a value between 1-100")
 
-    gridRainbow(16);
+    if (size > 100){
+        alert("Value is too large, please enter a maximum value of 100")
+    }else if (size < 1){
+        alert("Value is too small. Please enter a minimum value of 1")
+    }else{
 
+        function removeElements(){
+            const divNode = document.querySelectorAll("div.canvas");
+
+            divNode.forEach(element =>{
+                element.remove();
+            });
+        }
+        removeElements();
+        gridRainbow(size);
+    }
+    
 })
