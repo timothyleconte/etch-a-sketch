3738db443ac842c64745b3ab6065607d08930be3
timothyleconte <marktbooher@outlook.com>
2025-07-03 20:14:52 -0400

    Add formula for size argument to work in loop

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const container = document.querySelector (".container");
 function grid(size){
-    for (let i = 1; i < size; i++){
+
+    let newSize = (size * size) + 1
+    // let flex = `100 / ${size}`
+
+    for (let i = 1; i < newSize; i++){
+        
         const div = document.createElement("div");
         div.addEventListener("mouseover", () =>{
             div.style.backgroundColor = "blue";
@@ -11,19 +16,19 @@ function grid(size){
  
 }
 
-grid(257);
+grid(16);
 
 const button = document.querySelector(".selectSize");
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
 
-    let size = prompt("What grid size would you like?", "Enter a value between 1-100")
+//     let size = prompt("What grid size would you like?", "Enter a value between 1-100")
 
-    if (size > 100){
-        alert("Value is too large, please enter a max value of 100")
-    }else if (size < 1){
-        alert("Value is too small. Please enter a minimum value of 1")
-    }else{
-        
-    }
-})
+//     if (size > 100){
+//         alert("Value is too large, please enter a max value of 100")
+//     }else if (size < 1){
+//         alert("Value is too small. Please enter a minimum value of 1")
+//     }else{
+
+//     }
+// })
