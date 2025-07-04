8892b8abdaa6cfc0e7ddf433385196d153eea2bf
timothyleconte <marktbooher@outlook.com>
2025-07-03 22:19:10 -0400

    Add button functionality and logic
    Button is working as intended and logic works; however, I can't get the previous function to clear at this time to start a new grid.

--- javascript.js
+++ javascript.js
@@ -20,15 +20,15 @@ grid(16);
 
 const button = document.querySelector(".selectSize");
 
-// button.addEventListener("click", () => {
+button.addEventListener("click", () => {
 
-//     let size = prompt("What grid size would you like?", "Enter a value between 1-100")
+    let size = prompt("What grid size would you like?", "Enter a value between 1-100")
 
-//     if (size > 100){
-//         alert("Value is too large, please enter a max value of 100")
-//     }else if (size < 1){
-//         alert("Value is too small. Please enter a minimum value of 1")
-//     }else{
-
-//     }
-// })
+    if (size > 100){
+        alert("Value is too large, please enter a max value of 100")
+    }else if (size < 1){
+        alert("Value is too small. Please enter a minimum value of 1")
+    }else{
+        grid(size);
+    }
+})
